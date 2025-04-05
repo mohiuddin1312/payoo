4e387a1b1ea86120bf3a5ece8c5469c3c082f976
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-04-05 12:25:27 +0600

    home.html & home.js

--- js/login.js
+++ js/login.js
@@ -1,20 +1,35 @@
 // console.log("click the js file");
 //step-1 set event handler
+// document
+//   .getElementById("btn-login")
+//   .addEventListener("click", function (event) {
+//     // step-2 prevent default behavior(Prevent reloding browser )
+//     event.preventDefault();
+//     console.log("click the js file");
+
+//     //step-3 get the Phone number
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     console.log(phoneNumber, pinNumber);
+//     if (phoneNumber === "5" && pinNumber === "1234") {
+//       console.log("you are login");
+//       window.location.href = "/home.html";
+//     } else {
+//       alert("you have wrong information");
+//     }
+//   });
+
 document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
-    // step-2 prevent default behavior(Prevent reloding browser )
     event.preventDefault();
-    console.log("click the js file");
-
-    //step-3 get the Phone number
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
     console.log(phoneNumber, pinNumber);
-    if (phoneNumber === "5" && pinNumber === "1234") {
-      console.log("you are login");
+    if (phoneNumber === "78" && pinNumber === "9999") {
       window.location.href = "/home.html";
+      console.log("you are login");
     } else {
-      alert("you have wrong information");
+      alert("your information is wrong");
     }
   });
