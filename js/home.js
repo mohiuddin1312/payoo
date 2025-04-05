9379d686fabe13f26f9d7c2def4e35b05ca61fd6
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-04-05 15:07:38 +0600

    balance

--- js/home.js
+++ js/home.js
@@ -11,4 +11,20 @@ document
     console.log(inputAddMoney);
     const inputPin = document.getElementById("input-pin-number").value;
     console.log(inputPin);
+    // step-3 varify pin numbe
+    // wrong way to validate pin number.
+    if (inputPin === "222") {
+      console.log("add money to your acount");
+      const balance = document.getElementById("accound-balance").innerText;
+      console.log(balance);
+      // step-4 add money input with balace.
+      const addMoneyNumber = parseFloat(inputAddMoney);
+      const balanceNumber = parseFloat(balance);
+      const newbalance = balanceNumber + addMoneyNumber;
+      console.log(newbalance);
+      //   step-5 Update the balance
+      document.getElementById("accound-balance").innerText = newbalance;
+    } else {
+      alert("Fail to add money. please try again");
+    }
   });
