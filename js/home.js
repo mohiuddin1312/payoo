0d203ec3f23264e360cbec77adb4f231d3e1228c
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-04-05 13:02:44 +0600

    incomplite home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log("home added");
+// add money to the acount
+
+// step-1 add event handler to the add money button inside form
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    // prevent page after relode submit.
+    event.preventDefault();
+    // step-2 input add money
+    const inputAddMoney = document.getElementById("input-add-money").value;
+    console.log(inputAddMoney);
+    const inputPin = document.getElementById("input-pin-number").value;
+    console.log(inputPin);
+  });
